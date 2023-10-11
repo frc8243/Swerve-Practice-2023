604b02391089a4faa58183fd008fc86cba806baa
JulienM14 <julienmedina314@gmail.com>
2023-10-10 20:16:23 -0400

    why do i always have to double commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,36 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Gyro;
+import frc.robot.subsystems.Vision;
 
 public class RobotContainer {
+  private static RobotContainer m_robotContainer = new RobotContainer();
+  /* Subsystem Creation */
+  public static Drivetrain m_drivetrain;
+  public static Gyro m_gyro;
+  public static Vision m_vision;
+  /* PDH in Code to allow for data logging*/
+  public static PowerDistribution m_pdp;
+  /* Controllers */
+  private final CommandXboxController driverController = new CommandXboxController(0);
   public RobotContainer() {
+    m_drivetrain = new Drivetrain();
+    m_gyro = new Gyro();
+    m_pdp = new PowerDistribution(1, ModuleType.kRev);
+    m_vision = new Vision();
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
